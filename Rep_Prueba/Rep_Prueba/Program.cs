dbd2a80a7759d50e4d3a79fb35eae7d09c6ac619
JhonMaicol2003 <DevMike@programador.com>
2023-08-06 13:04:11 -0500

    Se a realizado una prueba en el proyecto generado recientemente.

---
+++ Rep_Prueba/Rep_Prueba/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rep_Prueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esta es una prueba del repositorio de GitHub :)");
+        }
+    }
+}
